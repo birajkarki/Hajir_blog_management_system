d67476491a0c2fa66401d8abf39920a0aed300dc
Sandip <sandipstha139@gmail.com>
2024-06-05 09:23:09 +0545

    sfa

--- Frontend/src/utils/apiRequest.js
+++ Frontend/src/utils/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const ApiRequest = axios.create({
-    baseURL: "http://localhost:3000/api/v1",
+    baseURL: "http://localhost:4000/api/v1",
     // baseURL: "https://backendhajir-blogmanagement.onrender.com/api/v1",
     withCredentials: true,
     // headers: {
