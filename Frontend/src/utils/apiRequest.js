9375c4ddd785cc5ad670614f38ce52f067a56e18
Biraj Karki <birajkarki9849@gmail.com>
2024-06-18 01:31:29 +0545

    added

--- Frontend/src/utils/apiRequest.js
+++ Frontend/src/utils/apiRequest.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const ApiRequest = axios.create({
-    baseURL: "http://localhost:4000/api/v1",
-    // baseURL: "https://backendhajir-blogmanagement.onrender.com/api/v1",
+    // baseURL: "http://localhost:4000/api/v1",
+    baseURL: "https://backendhajir-blogmanagement.onrender.com/api/v1",
     withCredentials: true,
     // headers: {
     //     'Content-Type': 'multipart/form-data',
