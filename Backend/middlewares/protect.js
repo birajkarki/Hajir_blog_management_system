285b8f7e321ad27efcdb7cf49f99a95b571c82c2
Sandip <sandipstha139@gmail.com>
2024-06-18 09:08:24 +0545

    fdsafas

--- Backend/middlewares/protect.js
+++ Backend/middlewares/protect.js
@@ -6,16 +6,11 @@ import jwt from "jsonwebtoken";
 const protect = CatchAsync(async (req, res, next) => {
   let token;
 
-<<<<<<< HEAD
-  token = req.cookies.jwt;
- 
-=======
   
     token = req.cookies.jwt;
   
 
   console.log("token", token)
->>>>>>> b778e9fe03a3dd69e4632fbe8d51fbe22e59296e
 
   if (!token) {
     return next(new AppError("Not Authorized", 401));
