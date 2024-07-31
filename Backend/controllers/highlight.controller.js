1b26b87fd3ae942f2fa6384f29b9045c28300f33
Sudan Basyal <sudanbasyal74@gmail.com>
2024-07-31 12:24:08 +0545

    pray for this backend to work because idk if it works or not

--- Backend/controllers/highlight.controller.js
+++ Backend/controllers/highlight.controller.js
@@ -3,7 +3,7 @@ import AppError from "../utils/AppError.js";
 import { CatchAsync } from "../utils/catchAsync.js";
 export const createHighlight = CatchAsync(async (req, res, next) => {
   const { blogId } = req.obj;
-  res.json({ files: req.files });
+
   const imageUrls = await Promise.all(
     req.files.map((file) => {
       return file.filename;
@@ -23,7 +23,9 @@ export const createHighlight = CatchAsync(async (req, res, next) => {
       highlightImageDescription,
       highlightImageCaption,
     } = value;
-    const image = imageUrls[i];
+    const image = `${req.protocol}://${req.get("host")}/uploads/${
+      imageUrls[i]
+    }`;
     const id = i + 1;
     return {
       id,
@@ -36,25 +38,12 @@ export const createHighlight = CatchAsync(async (req, res, next) => {
     };
   });
   highlightSections = sectionData;
-  //   console.log(highlightSections);
   try {
-    res.json({
+    const highlight = await Highlight.create({
       highlightTitle,
       highlightDescription,
       highlightSections,
       blogId,
-    });
-    // const highlight = await Highlight.create({
-    //   highlightTitle,
-    //   highlightDescription,
-    //   highlightSections,
-    //   blogId,
-    // });
-    highlight.highlightSections = highlight.highlightSections.map((section) => {
-      section.image = `${req.protocol}://${req.get("host")}/uploads/${
-        section.image
-      }`;
-      return section;
     });
     res.status(201).json({
       success: true,
@@ -87,71 +76,91 @@ export const updateHighlight = CatchAsync(async (req, res, next) => {
   const { blogId } = req.obj;
   let imageUrls = [];
   let highlightSectionData = [];
+  if (
+    req.body.highlightSections &&
+    req.files &&
+    req.files.highlightSectionImages
+  ) {
+    imageUrls = await Promise.all(
+      req.files.highlightSectionImages.map((file) => {
+        return `${req.protocol}://${req.get("host")}/uploads/${file.filename}`;
+      })
+    );
+  }
+
+  // try {
+  // Check if new images are provided
+
+  //   // Retrieve the existing highlight
+  const existingHighlight = await Highlight.findOne({ where: { blogId } });
+  if (!existingHighlight) {
+    return res.status(404).json({
+      success: false,
+      message: "Highlight not found",
+    });
+  }
+
+  existingHighlight.highlightTitle = req.body.highlightTitle
+    ? req.body.highlightTitle
+    : existingHighlight.highlightTitle;
+  existingHighlight.highlightDescription = req.body.highlightDescription
+    ? req.body.highlightDescription
+    : existingHighlight.highlightDescription;
+
+  if (req.body.highlightSections) {
+    const newHighlightSections = JSON.parse(req.body.highlightSections);
+    highlightSectionData = JSON.parse(existingHighlight.highlightSections).map(
+      (highlightSection) => {
+        let existingHighlightSection = highlightSection;
+        // console.log(existingHighlightSection);
+        const id = existingHighlightSection.id;
+        newHighlightSections.forEach((newHighlightSection, i) => {
+          if (id === newHighlightSection.id) {
+            console.log(newHighlightSection);
+            existingHighlightSection.title = newHighlightSection.title
+              ? newHighlightSection.title
+              : existingHighlightSection.title;
+            existingHighlightSection.description =
+              newHighlightSection.description
+                ? newHighlightSection.description
+                : existingHighlightSection.description;
+            existingHighlightSection.highlightImageAltText =
+              newHighlightSection.highlightImageAltText
+                ? newHighlightSection.highlightImageAltText
+                : existingHighlightSection.highlightImageAltText;
+            existingHighlightSection.highlightImageDescription =
+              newHighlightSection.highlightImageDescription
+                ? newHighlightSection.highlightImageDescription
+                : existingHighlightSection.highlightImageDescription;
+            existingHighlightSection.highlightImageCaption =
+              newHighlightSection.highlightImageCaption
+                ? newHighlightSection.highlightImageCaption
+                : existingHighlightSection.highlightImageCaption;
+            if (
+              req.files.highlightSectionImages &&
+              req.files.highlightSectionImages.length > 0 &&
+              imageUrls[i]
+            ) {
+              existingHighlightSection.image = imageUrls[i];
+            }
+          }
+        });
+        return { ...existingHighlightSection };
+      }
+    );
+  }
+
+  existingHighlight.highlightSections = highlightSectionData.length
+    ? highlightSectionData
+    : existingHighlight.highlightSections;
 
   try {
-    // Check if new images are provided
-    if (
-      req.body.highlightSections &&
-      req.files &&
-      req.files.highlightSectionImages
-    ) {
-      imageUrls = await Promise.all(
-        req.files.highlightSectionImages.map((file) => {
-          return file.filename;
-        })
-      );
-    }
-    //   // Retrieve the existing highlight
-    const existingHighlight = await Highlight.findOne({ where: { blogId } });
-    if (!existingHighlight) {
-      return res.status(404).json({
-        success: false,
-        message: "Highlight not found",
-      });
-    }
-
-    existingHighlight.highlightTitle = req.body.highlightTitle
-      ? req.body.highlightTitle
-      : existingHighlight.highlightTitle;
-    existingHighlight.highlightDescription = req.body.highlightDescription
-      ? req.body.highlightDescription
-      : existingHighlight.highlightDescription;
-
-    if (req.body.highlightSections) {
-      try {
-        const newHighlightSections = JSON.parse(req.body.highlightSections);
-        highlightSectionData = JSON.parse(
-          existingHighlight.highlightSections
-        ).map((section, index) => {
-          const newHighlightSection = newHighlightSections[index];
-          section.title = newHighlightSection.title || section.title;
-          section.description =
-            newHighlightSection.description || section.description;
-          if (imageUrls[index]) {
-            section.image = imageUrls[index];
-          }
-          return section;
-        });
-      } catch (error) {
-        console.error("Error parsing highlight sections JSON:", error);
-        return res.status(400).json({
-          success: false,
-          message: "Invalid highlight sections format",
-        });
-      }
-    }
-
-    existingHighlight.highlightSections = highlightSectionData.length
-      ? highlightSectionData
-      : existingHighlight.highlightSections;
-
-    // const updatedHighlight = await existingHighlight.save();
-
+    const updatedHighlight = await existingHighlight.save();
     res.status(200).json({
       success: true,
       message: "Highlight updated successfully",
-      // highlight: updatedHighlight,
-      existingHighlight,
+      highlight: updatedHighlight,
+      // existingHighlight,
     });
   } catch (error) {
     if (imageUrls.length > 0) {
